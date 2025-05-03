a0691eccaae07fd4cbef6892c11173f9d403ffd7
student <inho524890@gmail.com>
2025-05-03 11:54:29 +0000

    modify evaluate.sh to test skeleton code

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -2,9 +2,9 @@
 rm -rf /workspace/evaluate/*
 
 # emulate submission process
-cp -r /home/student/workspace/arc /workspace/evaluate
-cp -r /home/student/workspace/artifacts /workspace/evaluate
-cp /home/student/workspace/setup.sh /workspace/evaluate
+cp -r /home/student/workspace/skeleton/arc /workspace/evaluate
+cp -r /home/student/workspace/skeleton/artifacts /workspace/evaluate
+cp /home/student/workspace/skeleton/setup.sh /workspace/evaluate
 
 cd /workspace/evaluate
 source /workspace/evaluate/setup.sh
