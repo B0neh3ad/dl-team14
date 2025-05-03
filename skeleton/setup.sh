2213301268d8fabc1be7a9d77a7894e3eead4ddb
student <inho524890@gmail.com>
2025-05-03 11:54:00 +0000

    activate setup.sh

--- skeleton/setup.sh
+++ skeleton/setup.sh
@@ -10,6 +10,7 @@
 # and installing packages from scratch takes a lot of time,
 # that can impact time limit of evaluation.
 # 
-# export EVAL_ENV=testenv
-# conda create -n $EVAL_ENV python=3.10 -y
-# conda run -n $EVAL_ENV pip install -r /workspace/evaluate/artifacts/requirements.txt
+export HF_TOKEN=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
+export EVAL_ENV=testenv
+conda create -n $EVAL_ENV python=3.10 -y
+conda run -n $EVAL_ENV pip install -r /workspace/evaluate/artifacts/requirements.txt
