5a9feb23efa794adb837b97e2a815eda5b1b27eb
B0neh3ad <js1044k@gmail.com>
2025-05-09 23:32:12 +0900

    update train.sh args

--- skeleton/train.sh
+++ skeleton/train.sh
@@ -4,4 +4,4 @@ source /home/student/workspace/skeleton/setup.sh
 
 eval "$(conda shell.bash hook)"
 conda activate $EVAL_ENV
-python train.py --debug
+python train.py --lora-r 128 --learning-rate 5e-5 --train-batch-size 4 --grad-acc-steps 4
