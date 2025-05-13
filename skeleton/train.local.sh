8f15741fe75e3071715386a9dff68e960a27e7a1
B0neh3ad <js1044k@gmail.com>
2025-05-13 21:41:54 +0900

    update shell fiiles

--- skeleton/train.local.sh
+++ skeleton/train.local.sh
@@ -4,4 +4,4 @@ source setup.sh
 
 eval "$(conda shell.bash hook)"
 conda activate $EVAL_ENV
-python train.py --lora-r 128 --learning-rate 5e-5 --train-batch-size 4 --grad-acc-steps 4
+python train.py "$@"
